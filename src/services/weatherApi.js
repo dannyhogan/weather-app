efc894359a10d233e2523ff1fb964f8115d53853
Daniel Hogan <hogan.daniel@hotmail.com>
2020-05-02 11:58:08 -0700

    fix fetch

--- src/services/weatherApi.js
+++ src/services/weatherApi.js
@@ -1,20 +1,20 @@
 export const getWeatherData = (lat, long) => {
-  console.log('in getWeatherData', lat, long)
+  console.log("in getWeatherData", lat, long);
   // return fetch(`https://api.weather.gov/points/39.7456,-97.0892`)
   return fetch(`https://api.weather.gov/points/${lat},${long}`, {
-    mode: 'cors', // no-cors, *cors, same-origin
+    mode: "cors", // no-cors, *cors, same-origin
   })
-    .then(res => ([res.ok, res.json()]))
+    .then((res) => [res.ok, res.json()])
     .then(([ok, json]) => {
-      if(!ok) throw `Unable to get weather data for ${lat}, ${long}.`;
+      if (!ok) throw `Unable to get weather data for ${lat}, ${long}.`;
       return json;
     })
-    .then(weather => {
+    .then((weather) => {
       return fetch(weather.properties.forecast)
-        .then(res => res.json())
-        .then(forecast => {
-          console.log(forecast)
+        .then((res) => res.json())
+        .then((forecast) => {
+          console.log(forecast);
           return forecast.properties.periods;
         });
-    })
+    });
 };
